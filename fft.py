7dd33c32a0e7832b4fe0488ebb0440934f571aab
Rob Lytton <92745408+RobLytton@users.noreply.github.com>
2023-05-08 22:27:36 -0400

    Add files via upload

--- fft.py
+++ fft.py
@@ -1,32 +1,78 @@
+import sys
+f = open('data_in.sim', 'r')
+from playsound import playsound
+from socket import *
 import time
+from scipy.io import wavfile
 import numpy as np
+import os
+
 address = ('192.168.1.177', 5000)
-data = [13,21,17,2,-4,67,83,-90,28,-21,5,39,-6]
 s = socket(AF_INET, SOCK_DGRAM)
 s.settimeout(1)
-trans = np.fft.fft(data);
+print(sys.argv[1])
+if(int(len(sys.argv)) == 2):
+    songFile = str(sys.argv[1]) + '.wav'
+else:
+    songFile = str('rick.wav')
+
+sampRate, song = wavfile.read(songFile)
 idx = 0
-ready = -1
-maxVal = max(abs(trans));
+readyNextPart = -1
+readyNextBit = -2
+perSec = 1
+samps = int(np.floor(sampRate / perSec))
+matrixLength = 64
+sampAve = int(np.floor(samps / matrixLength))
+firstString  = "Start"
+sendIdx = 0
+print("Sampling rate: " + str(sampRate))
+print("Samps per update: " + str(samps))
+print("Matrix Length: " + str(matrixLength))
+print("Samps per column: " + str(sampAve))
+print("Updating every 1/" + str(perSec) + " seconds" )
+print("Playing song")
 
-s.sendto(str(-1 * maxVal).encode(), address)
-
-while(1):
-
-    try:
+playsound(songFile, block = False)
+s.sendto(firstString.encode(), address)
+while idx < len(song):
+    
         rec_data, addr = s.recvfrom(2048)
-        if(int(rec_data) == ready):
+    
+    
+        if(int(rec_data) == int(readyNextPart)):
+            
+            tempData = song[idx * samps:idx * samps + samps];
+            
+            preFft = np.fft.fft(tempData);
+            cut = (sampAve * matrixLength)
+            
+            
+            tempCube = preFft[0:cut,0].reshape(matrixLength, sampAve);
+            
+            idx = idx + 1;
+            tempFft = np.zeros(matrixLength)
+            for i in range(matrixLength):
+                tempFft[i] = np.mean(abs(tempCube[i,:]))
+            
+            
 
-            string = str(abs(trans[idx]))
-            print(string)
-            s.sendto(string.encode(), address)
+            fft = tempFft;
+            
+            sendMax = str(-1 * max((tempFft)))
+            s.sendto(sendMax.encode(), address)
 
-
-            idx = idx + 1
-            idx = idx % len(data)
+        elif(int(rec_data) ==  int(readyNextBit)):
+            print("sending bit")
+            sendVal = str(fft[sendIdx]);
+            s.sendto(sendVal.encode(), address)
+            sendIdx = sendIdx + 1;
+            sendIdx = sendIdx % matrixLength
 
         else:
-             print(rec_data)
-    except:
-        print("No data")
-        pass
+            send = "ERROR" 
+            s.sendto(send.encode(), address)
+    
+
+        
+
